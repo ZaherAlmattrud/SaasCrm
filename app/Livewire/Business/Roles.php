152ac861e50f11d0ea5b2d60ee3c3fd85ae38741
ZaherAlmattrud <zaheralmattrud@gmail.com>
2025-04-15 10:27:19 +0300

    create role crud function and view

--- app/Livewire/Business/Roles.php
+++ app/Livewire/Business/Roles.php
@@ -3,14 +3,112 @@
 namespace App\Livewire\Business;
 
 use Livewire\Component;
-use Livewire\Attributes\Layout;
+
+use App\Models\Role;
 
 class Roles extends Component
 {
 
-    #[Layout('layouts.app')]
+    public $business_roles;
+    public $name;
+
+    /**
+     * Initializes the component by retrieving all roles.
+     *
+     * @return void
+     */
+    public function mount()
+    {
+
+        $this->business_roles = Role::all();
+    }
+
+    /**
+     * Renders the roles component.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
     public function render()
     {
         return view('livewire.business.roles');
+    }
+
+
+    /**
+     * Creates a new role in the database.
+     *
+     * @return void
+     */
+
+    public function create()
+    {
+
+        Role::create([
+            'name' => $this->name,
+            'business_id'=>session('businessId'),
+        ]);
+
+        $this->resetInputFields();
+    }
+    /**
+     * Edit the specified role.
+     *
+     * This method retrieves a role by its ID and sets the name property
+     * to the role's name for editing purposes.
+     *
+     * @param int $id The ID of the role to be edited.
+     */
+
+
+    public function edit($id)
+    {
+
+        $role = Role::findOrFail($id);
+        $this->name = $role->name;
+    }
+
+    /**
+     * Updates the name of a role.
+     *
+     * This method retrieves a role by its ID and updates its name 
+     * with the current input. After updating, it resets the input fields.
+     *
+     * @param int $id The ID of the role to update.
+     */
+
+    public function update($id)
+    {
+
+        $role = Role::findOrFail($id);
+        $role->update([
+            'name' => $this->name
+        ]);
+        $this->resetInputFields();
+    }
+    
+    /**
+     * Deletes a role by ID.
+     *
+     * This method will look for a role with the given ID and delete it.
+     *
+     * @param int $id The ID of the role to delete.
+     */
+    public function delete($id) {  
+
+        Role::findOrFail($id)->delete();
+         
+    }
+
+    /**
+     * Resets the input fields for the role form.
+     *
+     * This method clears the current role name input and refreshes the list of all roles.
+     */
+
+    public function resetInputFields()
+    {
+
+        $this->name = '';
+        $this->business_roles = Role::all();
     }
 }
--- resources/views/livewire/business/roles.blade.php
+++ resources/views/livewire/business/roles.blade.php
@@ -1,3 +1,32 @@
-<div>
-     Roles
+<div class="p-6">
+    <div class="flex">
+        <x-input type="text" wire:model="name" placeholder="Role Name"></x-input>
+        <x-button wire:click="create" >Create</x-button>
+    </div>
+
+   
+    <table class="bg-white rounded mt-10 w-full">
+        <tr class="bg-state-50 border">
+
+            <td class="p-2">Name</td>
+            <td class="p-2">Action</td>
+
+        </tr>
+
+        @foreach ($business_roles as $business_role)
+        <tr>
+
+            <td class="p-2"> {{ $business_role->name }} </td>
+            <td>
+                <button class="text-blue-500 cursor-pointer" wire:click="edit({{$business_role->id}})">Edit</button>
+                <button class="text-red-500 cursor-pointer" wire:click="delete({{$business_role->id}})">Delete</button>
+            </td>
+
+        </tr>
+        @endforeach
+
+    </table>
+
+ 
+
 </div>
